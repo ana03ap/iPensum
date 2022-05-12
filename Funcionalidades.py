6bab51b4a2e8918c7529d72baa1b15338b0c161a
Ana Ardila <90521002+ana03ap@users.noreply.github.com>
2022-05-12 12:08:42 -0500

    .

--- Funcionalidades.py
+++ Funcionalidades.py
@@ -19,13 +19,11 @@ class Login():# no sirve
 <<<<<<< HEAD
         self.text=text
     """
-
-    def printear(self):
-        print("hola")
-
+    
     def login(self, entradaUser, entradaCode, ventana, errorC, txtL, MenuBusqueda, InvalidCode):
-        self.user = self.entradaUser.get()
-        self.code = self.entradaCode.get()
+        """
+        self.user = entradaUser.get()
+        self.code = entradaCode.get()
         if(self.user == "" or self.code == ""):  # Campos vacios
             self.Error = tk.Label(ventana, image=errorC,
                                   borderwidth=0, width=190)
@@ -52,7 +50,22 @@ class Login():# no sirve
                                  borderwidth=0, width=194)
                 # sino, el sale una advertencia
                 Error.place(x=440, y=394, height=45)
+         """
+        user = entradaUser.get()
+        code = entradaCode.get()
+        if(user == "" or code == ""):  # Campos vacios
+            Error = tk.Label(ventana, image=errorC, borderwidth=0, width=190)
+            Error.place(x=447, y=394, height=45)
 
+        else:  # sino esta vacio
+            # Buscar en el txt que ese código sea de la universidad del Norte
+            if code in txtL:
+                MenuBusqueda()  # Si el código se encuentra que pase a la siguiente
+            else:
+                Error = tk.Label(ventana, image=InvalidCode,
+                                 borderwidth=0, width=194)
+                # sino, el sale una advertencia
+                Error.place(x=440, y=394, height=45)
     # VALIDAR QUE SOLO META NÚMEROS AL CODIGO ESTUDIANTIL
     # menu logeo
     """
